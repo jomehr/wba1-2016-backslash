0d825b1d0c75189bf2dae62c1b34c88c1f925e45
Michael Michel <mail@michael-michel.com>
2016-11-03 11:41:05 +0100

    Wiederhergestellt

--- T13-Datenstruktur_Content/API/BS_API.js
+++ T13-Datenstruktur_Content/API/BS_API.js
@@ -34,9 +34,9 @@ json_request.open("GET", highscoreData,true );
 json_request.send();
 }
 
-//Holt Daten mit der passenden ID
+//Holt Quiz mit der passenden ID
 function getQuizByID(quizID){
-    var highscoreData = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T13-Datenstruktur_Content/JSON/highscore.json";
+    var quizData = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T13-Datenstruktur_Content/JSON/quiz.json";
     var json_request = new XMLHttpRequest();
 
     json_request.onreadystatechange = function(){
@@ -71,13 +71,26 @@ function getQuizView(){
     var quizView = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T13-Datenstruktur_Content/JSON/uebersichtQuiz.json";
     var json_request = new XMLHttpRequest();
 
-            //for(var i=0;i<jsonData.highscore.length-1;i++){
-                jsonOut = JSON.stringify({position: jsonData[quizID].highscore[quizID].position,
-                                          name: jsonData[quizID].highscore[quizID].name,
-                                          punktzahl: jsonData[quizID].highscore[quizID].punktzahl});
-            //}
-            $( document ).trigger( "onQuizData", [ jsonOut ] );
+    json_request.onreadystatechange = function(){
+        if(json_request.readyState == 4 && json_request.status === 200 ){
+            var jsonData = JSON.parse(json_request.responseText);
+            var jsonOut = {};
+            var quizArray = [];
+            
+            for(var i=0;i<3;i++){
+                quizArray.push({quizID: jsonData[i].quizID,
+                                titel: jsonData[i].titel,
+                                autor: jsonData[i].autor,
+                                datum: jsonData[i].datum,
+                                spielzahl: jsonData[i].spielzahl,
+                                bild: jsonData[i].bild,
+                                text: jsonData[i].text
+                });
+                jsonOut = JSON.stringify(quizArray);
+            }
+            
+            $( document ).trigger( "onQuizView", [ jsonOut ] );
         }};
-json_request.open("GET", highscoreData,true );
+json_request.open("GET", quizView,true );
 json_request.send();
 }
