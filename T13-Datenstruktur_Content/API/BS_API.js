b023aa85422a1e946ab5d94b23df3163349772c5
Michael Michel <michael.michel@smail.th-koeln.de>
2016-11-03 09:40:18 +0100

    BS_API.js hinzugefuegt

---
+++ T13-Datenstruktur_Content/API/BS_API.js
@@ -0,0 +1,56 @@
+//Eine der Funktionen, die im HTML Code abgerufen werden können
+//Wichtig ist die beachtung der Parameter des "triggers"
+function loadJSON(){
+    //Link zu der JSON mit den Daten
+    var highscoreData = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T13-Datenstruktur_Content/TESTING/highscore_test_1.json";
+    
+    //Erstellung einer XMLHttpRequest
+    var json_request = new XMLHttpRequest();
+
+    json_request.onreadystatechange = function(){
+        if(json_request.readyState == 4 && json_request.status === 200 ){
+            //console.log(json_request.responseText);
+            //Javascript Funktion JSON.parse zum parsen der JSON data
+            var jsonData = JSON.parse(json_request.responseText);
+            //Erstellung einer Variable jsonOut, die am Ende zurückgegeben wird
+            var jsonOut = {};
+
+            // jsonData Variable enthält nun die Datenstruktur
+            // und kann per jsonData.["Name"] zugegriffen werden
+            for(var i=0;i<jsonData.highscore.length;i++){
+                //Mit JSON.stringify werden Daten in jsonOut eingefügt
+                //im JSON Format
+                jsonOut = JSON.stringify({position: jsonData[i].position,
+                                          name: jsonData[i].name,
+                                          punktzahl: jsonData[i].punktzahl});
+            }
+            //Funktion zum Übergeben der angefragten Daten
+            //trigger benötigt den Eventnamen (selber Name in html)
+            //und die zu übergebenden Daten (jsonOut in diesem Fall)
+            $( document ).trigger( "onDataDelivered", [ jsonOut ] );
+        }
+    };
+json_request.open("GET", highscoreData,true );
+json_request.send();
+}
+
+//Holt Daten mit der passenden ID
+function getQuizByID(quizID){
+    var highscoreData = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T13-Datenstruktur_Content/TESTING/highscore_test_1.json";
+    var json_request = new XMLHttpRequest();
+
+    json_request.onreadystatechange = function(){
+        if(json_request.readyState == 4 && json_request.status === 200 ){
+            var jsonData = JSON.parse(json_request.responseText);
+            var jsonOut = {};
+
+            //for(var i=0;i<jsonData.highscore.length-1;i++){
+                jsonOut = JSON.stringify({position: jsonData[quizID].highscore[quizID].position,
+                                          name: jsonData[quizID].highscore[quizID].name,
+                                          punktzahl: jsonData[quizID].highscore[quizID].punktzahl});
+            //}
+            $( document ).trigger( "onQuizData", [ jsonOut ] );
+        }};
+json_request.open("GET", highscoreData,true );
+json_request.send();
+}
