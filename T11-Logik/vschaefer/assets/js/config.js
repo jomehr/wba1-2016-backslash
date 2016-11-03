14eea42feb2b3d6720655f9f86cc8f5aa8426d57
Sergej Ebel <sergej.ebel@smail.th-koeln.de>
2016-11-03 17:21:20 +0100

    korregieren des timers beim quiz

---
+++ T11-Logik/vschaefer/assets/js/config.js
@@ -0,0 +1,19 @@
+//Basis Configs
+var base = {
+	dataURL: '/assets/json',
+	quizDuration: 10
+};
+
+//URLs JSONs
+var dataUrls = {
+	dataQuizOverviewURL: base.dataURL + '/quiz-uebersicht.json',
+	dataQuestionsURL: base.dataURL + '/quiz-fragen.json'
+
+};
+
+//URLs HTML-Dateien
+var viewUrls = {
+	viewQuizStartURL: 'quiz-start.html',
+	viewQuizRundeURL: 'quiz-runde.html',
+	viewQuizEndeURL: 'quiz-ende.html'
+};
