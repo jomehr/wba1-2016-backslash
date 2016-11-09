e425cabd382d893c0a34e39480d5923d794673c5
Aaron Wreschino <aaron.wreschino@smail.th-koeln.de>
2016-11-09 15:34:57 +0100

    Updated highscore html 			test(); 		</script>Please enter the commit message for your changes. Lines starting

---
+++ T03-StatHighscore/js/main.js
@@ -0,0 +1,29 @@
+//TODO write var to store array of all PlayersElements
+// every single playerElement holds Position, PlayerName, Points
+// write a function to add all Player at once.
+// sort before add if neccessary.
+
+function test()
+{
+	var Position = 1;
+	var Player = "ME :D";
+	var Points = 50;
+
+	for(var i = 0; i < 20; i++){
+		addPlayer(Position, Player, Points);
+	}
+}
+
+/*
+ * adds a single element to the table
+ */
+function addPlayer(position, player, points) 
+{
+	var tableDiv = document.getElementById("player_table");
+	
+	var div = document.createElement('tr');
+	div.className = 'table_element';
+	div.innerHTML = '<th>'+position+'</th><th>'+player+'</th><th>'+points+'</th>';
+	
+	tableDiv.appendChild(div);
+}
