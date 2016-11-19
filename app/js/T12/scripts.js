a30658b00fff7c6bf8e9e2d72aab55a0894d40c7
nikcani <niklascanisius@me.com>
2016-11-19 23:30:50 +0100

    included but also cut animations

--- app/js/T12/scripts.js
+++ app/js/T12/scripts.js
@@ -1,52 +1,70 @@
-/*from: https://codepen.io/designcouch/pen/Atyop */
-/*Hamburgertransformation*/
+/*var time = 15; /!* how long the timer runs for *!/
+var initialOffset = '440';
+var i = 1;
+var interval = setInterval(function() {
+	$('.circle_animation').css('stroke-dashoffset', initialOffset-((-(time+i))*(initialOffset/time)));
+	$('.qr-timer-item').text(15-i);
+	if (i == time) {
+		clearInterval(interval);
+	}
+	i++;
+}, 1000);
+
+$(document).ready(function(){
+	var elem = document.getElementById("myBar");
+
+	var width = 0;
+	var id = setInterval(frame, 30);
+	function frame() {
+		/!*Übergabe der Data_Value nach score*!/
+		var score = document.getElementById("myBar").getAttribute("data-value");
+		/!*Testing
+		 var score = 60;
+		 *!/
+		if (width >= score) {
+			clearInterval(id);
+		} else {
+			width++;
+			elem.style.width = width + '%';
+			document.getElementById("label").innerHTML = width * 1  + '%';
+		}
+	}
+	window.setTimeout(move,1000);
+});*/
+
 $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
 });
-/*selfwritten*/
-function changeAnswerButtonColor(n) {
-	switch(n){
-		case "wrongAnswer1":
-			document.getElementById('wrongAnswer1').style.backgroundColor = '#cb0727';
-			document.getElementById('rightAnswer').className += " animated flashshort";
-			document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
-			document.getElementById('currentQuestionProgress').className += " animated fadeInShort";
-			document.getElementById('currentQuestionProgress').style.backgroundColor = '#cb0727';
-		break;
-		case "wrongAnswer2":
-			document.getElementById('wrongAnswer2').style.backgroundColor = '#cb0727';
-			document.getElementById('rightAnswer').className += " animated flashshort";
-			document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
-			document.getElementById('currentQuestionProgress').className += " animated fadeInShort";
-			document.getElementById('currentQuestionProgress').style.backgroundColor = '#cb0727';
-		break;
-		case "wrongAnswer3":
-			document.getElementById('wrongAnswer3').style.backgroundColor = '#cb0727';
-			document.getElementById('rightAnswer').className += " animated flashshort";
-			document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
-			document.getElementById('currentQuestionProgress').className += " animated fadeInShort";
-			document.getElementById('currentQuestionProgress').style.backgroundColor = '#cb0727';
-		break;
-		case "rightAnswer":
-			document.getElementById('rightAnswer').className += " animated flashshort";
-			document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
-			document.getElementById('currentQuestionProgress').className += " animated fadeInShort";
-			document.getElementById('currentQuestionProgress').style.backgroundColor = '#4ac725';
-		break;
-	}
-}
 
-/* from: http://stackoverflow.com/questions/29649643/how-to-create-a-circular-countdown-timer-using-html-css-or-javascript */
-var time = 15; /* how long the timer runs for */
-var initialOffset = '440';
-var i = 1;
-var interval = setInterval(function() {
-    $('.circle_animation').css('stroke-dashoffset', initialOffset-((-(time+i))*(initialOffset/time)));
-    $('h2').text(15-i);
-    if (i == time) {
-        clearInterval(interval);
-    }
-    i++;  
-}, 1000);
+
+/* Jorge H. F. Pereira did new one 
+ function changeAnswerButtonColor(n) {
+ switch(n){
+ case "wrongAnswer1":
+ document.getElementById('wrongAnswer1').style.backgroundColor = '#cb0727';
+ document.getElementById('rightAnswer').className += " animated flashshort";
+ document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
+ document.getElementById('currentQuestionProgress').style.backgroundColor = '#cb0727';
+ break;
+ case "wrongAnswer2":
+ document.getElementById('wrongAnswer2').style.backgroundColor = '#cb0727';
+ document.getElementById('rightAnswer').className += " animated flashshort";
+ document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
+ document.getElementById('currentQuestionProgress').style.backgroundColor = '#cb0727';
+ break;
+ case "wrongAnswer3":
+ document.getElementById('wrongAnswer3').style.backgroundColor = '#cb0727';
+ document.getElementById('rightAnswer').className += " animated flashshort";
+ document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
+ document.getElementById('currentQuestionProgress').style.backgroundColor = '#cb0727';
+ break;
+ case "rightAnswer":
+ document.getElementById('rightAnswer').className += " animated flashshort";
+ document.getElementById('rightAnswer').style.backgroundColor = '#4ac725';
+ document.getElementById('currentQuestionProgress').style.backgroundColor = '#4ac725';
+ break;
+ }
+ }
+ */
