bbd9abc102d72c665739974a8a10400be93be897
nikcani <niklascanisius@me.com>
2016-11-22 18:36:50 +0100

    added animated timer (still bugs a bit, but i needed to push)

--- app/js/T12/round.js
+++ app/js/T12/round.js
@@ -1,21 +1,15 @@
-// how long the timer runs
-var timeOffset = 15;
-var time = 0;
-
-var initialOffset = 440;
-var offset = 440;
-
+var timeOffset = 13,
+    time = 13,
+    initialOffset = 440;
 
 var interval = setInterval(function () {
-    offset += initialOffset / timeOffset;
+    $(".circle_animation").css("stroke-dashoffset", initialOffset - (i * initialOffset / timeOffset));
 
-    $(".circle_animation").css("stroke-dashoffset", offset);
-
-    time++;
-
-    if (time === timeOffset) {
-        time = 0;
-        offset = 440;
-        $(".circle_animation").css("stroke-dashoffset", offset);
+    if (time === 0) {
+        time = 15;
+        timeOffset = 15;
+        //$(".circle_animation").css("stroke-dashoffset", offset);
     }
+
+    time--;
 }, 1000);
