59a067b94236f536a9dcb8de1f049081ed91a3c8
Exokay88 <tobias.seemann@hotmail.com>
2016-11-23 09:36:45 +0100

    fix multiple view error

--- app/js/view.class.js
+++ app/js/view.class.js
@@ -1,6 +1,6 @@
 var Quizobject = {};
 var sessionobject = {};
-
+var prefstate = 0;
 function fnc_reloadssobject(callback) {
     sessionobject = {
         "points": sessionStorage.getItem('points'),
@@ -66,101 +66,123 @@ var view = {
     },
     pre_quizoverview: function (callback) {
       Quizobject = {};
+      prefstate = 0; // setting state before executing to stop multiple callbacks
       view.dat_QuizView(function(data){
-        Quizobject.quizsuggestion = data; //copy object
 
-        if (typeof callback == "function") {
-          callback(Quizobject);
+        if(prefstate === 0 ) {
+          Quizobject.quizsuggestion = data; //copy object
+
+          if (typeof callback == "function") {
+            callback(Quizobject);
+          }
         }
       });
     },
     pre_quizinfo: function (callback) {
       Quizobject = {};
+      prefstate = 1;
       view.dat_QuizViewByID(sessionobject.quizID,function(data){
-        Quizobject.quizinfo = data;
-        Quizobject.sessionobject = sessionobject;
+        if(prefstate === 1) {
+          Quizobject.quizinfo = data;
+          Quizobject.sessionobject = sessionobject;
 
-        if (typeof callback == "function") {
-            callback(Quizobject);
+          if (typeof callback == "function") {
+              callback(Quizobject);
+          }
         }
       });
     },
     pre_quizround: function (callback) {
         Quizobject = {};
+        prefstate = 2;
         view.dat_QuizByID(sessionobject.quizID,function(data){
-          Quizobject.quiz = data;
-          Quizobject.sessionobject = sessionobject;
-          Quizobject.quiz.titel = "Quiztitel"; //TODO: get from API
+          if(prefstate === 2) {
+            Quizobject.quiz = data;
+            Quizobject.sessionobject = sessionobject;
+            Quizobject.quiz.titel = "Quiztitel"; //TODO: get from API
 
-          if (typeof callback == "function") {
-              callback(Quizobject);
+            if (typeof callback == "function") {
+                callback(Quizobject);
+            }
           }
         });
     },
     pre_quizend: function (callback) {
         Quizobject = {};
+        prefstate = 3;
         fnc_reloadssobject(function (ready) {
             view.dat_QuizViewByID(sessionobject.quizID,function(data){
-              Quizobject.datquiz = data;
-              var quizInfo = {};
-              view.dat_getHighscorePositions(sessionobject.quizID, sessionobject.username, sessionobject.points, function(data){
-                $.extend(Quizobject.datquiz, data);
-                view.dat_QuizView(function(data){
-                  var nQuizObject = data; //copy object
-                  var quizInfo = data;
-                  var icount = 0;
-                  var ARQused = [];
-                  ARQused[0] = sessionobject.quizID;
-                  var iRand = Math.random();
+              if(prefstate === 3) {
+                Quizobject.datquiz = data;
+                var quizInfo = {};
+                view.dat_getHighscorePositions(sessionobject.quizID, sessionobject.username, sessionobject.points, function(data){
+                  if(prefstate === 3) {
+                    $.extend(Quizobject.datquiz, data);
+                    view.dat_QuizView(function(data){
+                      if(prefstate === 3) {
+                        var nQuizObject = data; //copy object
+                        var quizInfo = data;
+                        var icount = 0;
+                        var ARQused = [];
+                        ARQused[0] = sessionobject.quizID;
+                        var iRand = Math.random();
 
-                  //RAND Quiz außer aufgerufene Quiz.
-                  for (var i = 0; icount < 4; i++) {
-                      iRand = Math.round(Math.random() * ( quizInfo.length - 0 ));
-                      if (iRand !== 0) {
-                          iRand--;
-                      }
-                      while ((ARQused.indexOf("" + iRand) > -1)) {
-                          iRand = Math.round(Math.random() * ( quizInfo.length - 0 ));
-                          if (iRand !== 0) {
-                              iRand--;
-                          }
-                      }
+                        //RAND Quiz außer aufgerufene Quiz.
+                        for (var i = 0; icount < 4; i++) {
+                            iRand = Math.round(Math.random() * ( quizInfo.length - 0 ));
+                            if (iRand !== 0) {
+                                iRand--;
+                            }
+                            while ((ARQused.indexOf("" + iRand) > -1)) {
+                                iRand = Math.round(Math.random() * ( quizInfo.length - 0 ));
+                                if (iRand !== 0) {
+                                    iRand--;
+                                }
+                            }
 
-                      if (quizInfo[iRand].quizID !== sessionobject.quizID) { // <- Abfrage ob QuizID == Random ID ist wenn true neu generieren
-                          icount++;
-                          ARQused[icount] = quizInfo[iRand].quizID;
-                          nQuizObject[icount - 1] = quizInfo[iRand];
-                          if (quizInfo[iRand].bild === "/URL") {
-                              quizInfo[iRand].bild = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T10-Asset/Bilder/Bilder%20Klein/Stadion3-Klein.jpg";
-                          }
+                            if (quizInfo[iRand].quizID !== sessionobject.quizID) { // <- Abfrage ob QuizID == Random ID ist wenn true neu generieren
+                                icount++;
+                                ARQused[icount] = quizInfo[iRand].quizID;
+                                nQuizObject[icount - 1] = quizInfo[iRand];
+                                if (quizInfo[iRand].bild === "/URL") {
+                                    quizInfo[iRand].bild = "https://raw.githubusercontent.com/th-koeln/wba1-2016-backslash/master/T10-Asset/Bilder/Bilder%20Klein/Stadion3-Klein.jpg";
+                                }
+                            }
+                        }
+                        Quizobject.quizes = nQuizObject;
+                        Quizobject.quizes = Quizobject.quizes.slice(0, 4);
+                        $.extend(Quizobject.datquiz, sessionobject);
+                        if (typeof callback == "function") {
+                            callback(Quizobject);
+                        }
                       }
-                  }
-                  Quizobject.quizes = nQuizObject;
-                  Quizobject.quizes = Quizobject.quizes.slice(0, 4);
-                  $.extend(Quizobject.datquiz, sessionobject);
-                  if (typeof callback == "function") {
-                      callback(Quizobject);
+                    });
                   }
 
                 });
-              });
+              }
+
             });
         });
     },
     pre_highscore: function (callback) {
       Quizobject = {};
+      prefstate = 4;
       view.dat_HighscoreByID(sessionobject.quizID, function(data) {
-        Quizobject = data;
-        Quizobject.sessionobject = sessionobject;
+        if(prefstate === 4) {
+          Quizobject = data;
+          Quizobject.sessionobject = sessionobject;
 
-        if (typeof callback == "function") {
-                callback(Quizobject);
+          if (typeof callback == "function") {
+                  callback(Quizobject);
+          }
         }
       });
     },
     dat_QuizViewByID: function(quizID,callback){
       getQuizViewByID(quizID);
       $(document).on("onQuizViewByID", function (event, data) {
+
           if (typeof callback == "function") {
               callback(data);
           }
